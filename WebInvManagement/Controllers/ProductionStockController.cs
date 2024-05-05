d79145c354a7818257ea1a05e49213eeb538a2ba
damd1 <damd1@DESKTOP-I2CJ6UC>
2024-05-05 21:52:30 +0300

    ProductionStocks Controller (items) Pages: Home, Stocks, Report, User Layout: Added partitions, Deleted footer

--- WebInvManagement/Controllers/ProductionStockController.cs
+++ WebInvManagement/Controllers/ProductionStockController.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using WebInvManagement.Data;
+using WebInvManagement.Models;
 
 namespace WebInvManagement.Controllers
 {
     public class ProductionStockController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProductionStockController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<ProductionStock> stocks = _context.ProductionStocks.ToList();
+            return View(stocks);
         }
     }
 }
